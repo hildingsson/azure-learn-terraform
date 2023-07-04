e198f5e4457dd96363a93e2a5651e77ae90c68dc
Jimmy Hildingsson <jimmyhildingsson@Jimmys-MacBook-Pro.local>
2023-07-04 13:23:44 +0200

    Added code for RG module

---
+++ azureModules/resourceGroup/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "azurermResourceGroup" {
+  name = "${var.azurerm_resourcegroup_location_abbreviation}-${var.azurerm_resourcegroup_environment}-${var.azurerm_resourcegroup_location_short}-${var.azurerm_resourcegroup_service}-01"
+  location = var.azurerm_resourcegroup_location
+}
---
+++ azureModules/resourceGroup/variables.tf
@@ -0,0 +1,23 @@
+variable "azurerm_resourcegroup_location_short" {
+  type = string
+  default = "ne"
+}
+
+variable "azurerm_resourcegroup_environment" {
+  type = string
+  default = "dev"
+}
+
+variable "azurerm_resourcegroup_abbreviation" {
+  type = string
+  default = "rg"
+}
+
+variable "azurerm_resourcegroup_service" {
+  type = string
+}
+
+variable "azurerm_resourcegroup_location" {
+  type = string
+  default = "North Europe"
+}
